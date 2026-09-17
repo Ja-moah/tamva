65076bf6a1c5670c9d56c4a09dfe5cc3a0203317
Ja-moah <stanjaysolutions@gmail.com>
2026-09-17 04:55:53 +0000

    feat(partner): add institutional application credentials

--- apps/partner/admin.py
+++ apps/partner/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from apps.partner.models import Institution, InstitutionMembership
+from apps.partner.models import (
+    ApiCredential,
+    CredentialScope,
+    Institution,
+    InstitutionMembership,
+    PartnerApplication,
+    PartnerEnvironment,
+    WebhookEndpoint,
+)
 
 admin.site.register(Institution)
 admin.site.register(InstitutionMembership)
+admin.site.register(PartnerApplication)
+admin.site.register(PartnerEnvironment)
+admin.site.register(CredentialScope)
+admin.site.register(ApiCredential)
+admin.site.register(WebhookEndpoint)
